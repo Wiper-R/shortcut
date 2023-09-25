33035a66170fc10a0f036be862d11e9613e31af9
Wiper-R <rshivang12345@gmail.com>
2023-09-25 23:11:29 +0530

    Removed unused imports

--- app/manage/links/linkentry.tsx
+++ app/manage/links/linkentry.tsx
@@ -9,27 +9,30 @@ export default function LinkEntry({ id, url, title, createdAt }: PrismaLink) {
   title = title || "Untitled " + formatDateTimeToUTC(createdAt);
   return (
     <div className="bg-white py-3 px-5 rounded-lg shadow-sm flex justify-between gap-8">
-      <div className="flex gap-2 justify-between flex-col">
-        <div className="flex gap-2 items-center">
-          <img
-            src={`https://www.google.com/s2/favicons?domain=http://${urlObj.hostname || "localhost"}&sz=32`}
-            alt=""
-            className="w-10 rounded-full bg-white border self-start"
-          />
+      <div className="flex gap-6 justify-between">
+        <img
+          src={`https://www.google.com/s2/favicons?domain=http://${
+            urlObj.hostname || "localhost"
+          }&sz=32`}
+          alt=""
+          className="w-10 rounded-full bg-white border self-start"
+        />
+        <div className="flex flex-col gap-2">
           <h6 className="text-2xl font-semibold line-clamp-1 break-all hover:underline">
             <Link href={`/links/view/${id}`}>{title}</Link>
           </h6>
-        </div>
-        <div className="text-lg font-semibold ml-14">
-          <Link
-            href={`https://linkswift.com/l/${id}`}
-            className="text-primary line-clamp-1 break-all"
-          >
-            {`linkswift.com/l/${id}`}
-          </Link>
-          <Link href={url} className="line-clamp-1 break-all">
-            {trimProtocols(url)}
-          </Link>
+
+          <div className="flex flex-col leading-6 text-lg font-medium">
+            <Link
+              href={`https://linkswift.com/l/${id}`}
+              className="text-primary line-clamp-1 break-all"
+            >
+              {`linkswift.com/l/${id}`}
+            </Link>
+            <Link href={url} className="line-clamp-1 break-all">
+              {trimProtocols(url)}
+            </Link>
+          </div>
         </div>
       </div>
 
