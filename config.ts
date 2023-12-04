af4c9c80d940c9a8f585122dd204daeb2a4d872a
Wiper-R <rshivang12345@gmail.com>
2023-12-05 00:15:29 +0530

    chore: use zod for env validation

---
+++ config.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+  JWT_SECRET: z.string(),
+  DATABASE_URL: z.string().url(),
+  TOKEN_COOKIE_KEY: z.string()
+});
+
+const env = envSchema.parse(process.env);
+
+export default { ...env };
