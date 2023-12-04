70b697b2fd5e681c53bcbed07a5f824ee124c750
Wiper-R <rshivang12345@gmail.com>
2023-12-05 00:31:47 +0530

    chore: use session

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -5,25 +5,15 @@ import { cleanUser } from "@/lib/utils";
 import { errorResponse, successResponse } from "@/app/api/_response";
 import { NextRequest } from "next/server";
 import config from "@/config";
+import { getSession } from "@/auth/session";
 
 export async function GET(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
-  // TODO: Use session
-  const token = cookies().get(config.TOKEN_COOKIE_KEY)?.value;
-
-  const unauthorized = errorResponse(
-    { message: "Unauthorized" },
-    { status: 401 }
-  );
-
-  if (!token) return unauthorized;
-
-  const payload = decodeJwt(token);
-  const user = await prisma.user.findFirst({ where: { id: payload.sub } });
-
-  if (!user) return unauthorized;
-
-  return successResponse({ user: cleanUser(user) });
+  const session = await getSession();
+  if (session) {
+    return successResponse({ user: session.user });
+  }
+  return errorResponse({ message: "Unauthorized" }, { status: 401 });
 }
