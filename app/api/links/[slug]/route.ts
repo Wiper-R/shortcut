1e453956a90a80282958612b82621f5e0d610313
Wiper-R <rshivang12345@gmail.com>
2023-12-05 01:04:27 +0530

    fix: handle exceptions

--- app/api/links/[slug]/route.ts
+++ app/api/links/[slug]/route.ts
@@ -1,37 +1,46 @@
 import { NextRequest } from "next/server";
-import { errorResponse, successResponse } from "../../_response";
+import { successResponse } from "../../_response";
 import { editLinkSchema } from "@/validators/linksValidator";
 import prisma from "@/prisma";
-import { cleanShortenLink } from "@/lib/utils";
+import { cleanShortenLink, requiredRecordsNotFound } from "@/lib/utils";
 import { getSession } from "@/auth/session";
-import { unauthorized } from "../../_error-codes";
+import { somethingWentWrong, unauthorized } from "../../_error-codes";
 
 type Params = { params: { slug: string } };
-
-// TODO: Add exception handling
 
 export async function PATCH(request: NextRequest, { params }: Params) {
   const { slug } = params;
   const body = await request.json();
   const data = editLinkSchema.parse(body);
-  const check = await prisma.shortenLink.findFirst({ where: { slug } });
-
+  
   const session = await getSession();
   if (!session) return unauthorized();
 
-  if (!check)
-    return errorResponse({ message: "Link not found" }, { status: 404 });
+  try {
+    var shortenLink = await prisma.shortenLink.update({
+      where: { slug, userId: session.user.id },
+      data,
+    });
+  } catch (e) {
+    if (requiredRecordsNotFound(e)) return unauthorized();
 
-  const shortenLink = await prisma.shortenLink.update({
-    where: { slug, userId: session.user.id },
-    data,
-  });
+    return somethingWentWrong();
+  }
   return successResponse({ shortenLink: cleanShortenLink(shortenLink) });
 }
 
 export async function DELETE(request: NextRequest, { params }: Params) {
   const { slug } = params;
-  const shortenLink = await prisma.shortenLink.delete({ where: { slug } });
+  const session = await getSession();
+  if (!session) return unauthorized();
+  try {
+    var shortenLink = await prisma.shortenLink.delete({
+      where: { slug, userId: session.user.id },
+    });
+  } catch (e) {
+    if (requiredRecordsNotFound(e)) return unauthorized();
+    return somethingWentWrong();
+  }
   return successResponse(
     { shortenLink: cleanShortenLink(shortenLink) },
     { status: 202 }
