7a935d805c89c56f6a2c10c9a811af93fb0fa448
Wiper-R <rshivang12345@gmail.com>
2023-12-05 14:37:31 +0530

    feat: add qr-code api

---
+++ app/api/links/[slug]/qr-code/route.ts
@@ -0,0 +1,44 @@
+import errorCodes from "@/app/api/_error-codes";
+import { errorResponse, successResponse } from "@/app/api/_response";
+import { getSession } from "@/auth/session";
+import { isUniqueValidationError } from "@/lib/utils";
+import prisma from "@/prisma";
+import { createQrCodeSchema } from "@/validators/qrCodeValidator";
+import { NextRequest } from "next/server";
+
+type Params = { params: { slug: string } };
+
+export async function POST(request: NextRequest, { params: { slug } }: Params) {
+  const body = await request.json();
+  const data = createQrCodeSchema.parse(body);
+
+  const session = await getSession();
+  if (!session) return errorCodes.Unauthorized();
+
+  const shortenLink = await prisma.shortenLink.findFirst({
+    where: { slug },
+  });
+
+  if (!shortenLink) return errorCodes.NotFound();
+
+  if (shortenLink.userId != session.user.id) return errorCodes.Forbidden();
+
+  try {
+    var qrCode = await prisma.qrCode.create({
+      data: { shortenLinkId: shortenLink.id, ...data },
+    });
+  } catch (e) {
+    if (isUniqueValidationError(e)) return errorCodes.Conflict();
+
+    return errorCodes.Unknown();
+  }
+
+  return successResponse({ qrCode }, { status: 201 });
+}
+
+// export async function PATCH(
+//   request: NextRequest,
+//   { params: { slug } }: Params
+// ) {
+
+// }
