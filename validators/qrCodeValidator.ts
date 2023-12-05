c72f1cdde48129c67756912d751b695b6a482e58
Wiper-R <rshivang12345@gmail.com>
2023-12-05 15:04:32 +0530

    chore: made fgColor, bgColor required

--- validators/qrCodeValidator.ts
+++ validators/qrCodeValidator.ts
@@ -3,9 +3,9 @@ import { z } from "zod";
 export const createQrCodeSchema = z
 .object({
   // TODO: add hex validator
-  fgColor: z.string().optional(),
-  bgColor: z.string().optional(),
+  fgColor: z.string(),
+  bgColor: z.string(),
 
   // TODO: Test with base64 urls
-  icon: z.string().url().nullish(),
+  iconUrl: z.string().url().nullish(),
 })
