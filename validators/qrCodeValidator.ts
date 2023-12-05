b13a94f35e88b43a40d0a7ebbcc13495735d07f8
Wiper-R <rshivang12345@gmail.com>
2023-12-05 17:11:50 +0530

    chore: add QrcodeValidator

--- validators/qrCodeValidator.ts
+++ validators/qrCodeValidator.ts
@@ -1,11 +1,16 @@
 import { z } from "zod";
 
-export const createQrCodeSchema = z
-.object({
+export const createQrCodeSchema = z.object({
   // TODO: add hex validator
   fgColor: z.string(),
   bgColor: z.string(),
 
   // TODO: Test with base64 urls
   iconUrl: z.string().url().nullish(),
-})
+});
+
+export const updateQrCodeSchema = z.object({
+  fgColor: z.string().optional(),
+  bgColor: z.string().optional(),
+  iconUrl: z.string().url().nullish(),
+});
