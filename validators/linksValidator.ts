71a1d77283fc0a31d77f07bf62014b7ce438e0a7
Wiper-R <rshivang12345@gmail.com>
2023-12-05 10:01:33 +0530

    chore: add listLinkSchema

--- validators/linksValidator.ts
+++ validators/linksValidator.ts
@@ -1,16 +1,19 @@
 import { z } from "zod";
 
 // TODO: Implement schemas
-const createLinkSchema = z.object({
+export const createLinkSchema = z.object({
   title: z.string(),
   destination: z.string().url(),
   slug: z.string().min(1), // TODO: Add a slug validator
 });
 
-const editLinkSchema = z.object({
+export const editLinkSchema = z.object({
   destination: z.string().optional(),
   slug: z.string().optional(),
-  title: z.string().nullish().optional(),
+  title: z.string().nullable().optional(),
 });
 
-export { createLinkSchema, editLinkSchema };
+export const listLinkSchema = z.object({
+  limit: z.number().min(1).default(10),
+  cursor: z.string().nullable().optional(),
+});
