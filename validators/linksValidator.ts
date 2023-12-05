71f4aa21c97d6d584f28d03c3e6dd02e79aa13c0
Wiper-R <rshivang12345@gmail.com>
2023-12-05 13:52:07 +0530

    chore: add qrCode fields

--- validators/linksValidator.ts
+++ validators/linksValidator.ts
@@ -5,6 +5,17 @@ export const createLinkSchema = z.object({
   title: z.string(),
   destination: z.string().url(),
   slug: z.string().min(1), // TODO: Add a slug validator
+  generateQrCode: z.boolean().default(false),
+  qrCode: z
+    .object({
+      // TODO: add hex validator
+      fgColor: z.string().optional(),
+      bgColor: z.string().optional(),
+
+      // TODO: Test with base64 urls
+      icon: z.string().url().nullish(),
+    })
+    .optional(),
 });
 
 export const editLinkSchema = z.object({
