dc83a998b6b7403489599fb5030a7923a6575418
Wiper-R <rshivang12345@gmail.com>
2023-12-07 00:51:48 +0530

    feat: add engagement routes

---
+++ app/api/links/[slug]/engagements/route.ts
@@ -0,0 +1,30 @@
+import _errorCodes from "@/app/api/_error-codes";
+import { successResponse } from "@/app/api/_response";
+import prisma from "@/prisma";
+import { createEngagementSchema } from "@/validators/engagementsValidator";
+import { EngagementType } from "@prisma/client";
+import { NextRequest } from "next/server";
+
+type Params = {
+  params: { slug: string };
+};
+
+export async function POST(request: NextRequest, { params: { slug } }: Params) {
+  const body = await request.json();
+  const data = createEngagementSchema.parse(body);
+  const shortenLink = await prisma.shortenLink.findFirst({ where: { slug } });
+  if (!shortenLink) return _errorCodes.NotFound();
+  try {
+    var engagement = await prisma.engagement.create({
+      data: {
+        shortenLinkId: shortenLink.id,
+        type: EngagementType.visit,
+        ...data,
+      },
+    });
+  } catch (e) {
+    return _errorCodes.Unknown();
+  }
+
+  return successResponse({ engagement }, { status: 201 });
+}
