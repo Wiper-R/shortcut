7660f86987cccf501b09ef5899cbc819332cd104
Wiper-R <rshivang12345@gmail.com>
2023-12-27 12:22:35 +0530

    chore(ui): add navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import React from "react";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import Image from "next/image";
+
+import Shortcut from "@/public/shortcut.png";
+import Button from "./ui/Button";
+
+type NavLinkProps = {
+  label: string;
+  href: string;
+};
+
+function NavLink({ label, href }: NavLinkProps) {
+  return (
+    <Link href={href} className="font-semibold text-sm">
+      {label}
+    </Link>
+  );
+}
+
+function Navbar() {
+  return (
+    <MaxWidthWrapper className="mt-10 sticky">
+      <header className="flex py-5 px-8 shadow-md rounded-xl items-center justify-center border">
+        <Image src={Shortcut.src} width={24} height={24} alt="Shortcut Logo" />
+        <nav className="flex gap-10 ml-auto items-center">
+          <NavLink label="Home" href="/" />
+          <NavLink label="Docs" href="/docs" />
+          <NavLink label="Github" href="/github" />
+          <Button>Login</Button>
+        </nav>
+      </header>
+    </MaxWidthWrapper>
+  );
+}
+
+export default Navbar;
