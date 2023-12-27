fd9439fa37fb2fa8fde317d8f423e7d57be05d37
Wiper-R <rshivang12345@gmail.com>
2023-12-27 12:22:57 +0530

    chore(ui): add max width wrapper

---
+++ components/MaxWidthWrapper.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@/lib/utils";
+import { ClassValue } from "clsx";
+import { PropsWithChildren } from "react";
+
+type MaxWidthWrapperProps = {
+  className?: ClassValue;
+} & PropsWithChildren;
+
+export default function MaxWidthWrapper({
+  children,
+  className,
+}: MaxWidthWrapperProps) {
+  return <div className={cn("max-w-7xl mx-auto", className)}>{children}</div>;
+}
