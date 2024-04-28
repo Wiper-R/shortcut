836defe7fbb836a21a97e709568fb475474844e7
Shivang Rathore <rshivang12345@gmail.com>
2024-04-28 10:16:22 +0530

    fix: sidebar item hover

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -30,7 +30,7 @@ const SidebarItem = (props: SidebarItemProp) => {
     <Link
       href={_href}
       className={cn(
-        "flex items-center gap-2 rounded px-3 py-2 text-sm text-secondary-foreground transition-colors hover:bg-violet-100 hover:text-black hover:text-primary-foreground",
+        "flex items-center gap-2 rounded px-3 py-2 text-sm text-secondary-foreground transition-colors hover:bg-violet-100 hover:text-black",
         {
           "bg-violet-500 text-white hover:bg-violet-500 hover:text-white":
             pathname == _href,
