755f718868a5db38bb575e4837767384bced345c
Shivang Rathore <rshivang12345@gmail.com>
2024-05-02 12:16:13 +0530

    fix: session doesn't persist on browser restart

--- lib/server-utils.ts
+++ lib/server-utils.ts
@@ -1,13 +1,19 @@
 import config from "@/config";
-import { Prisma } from "@prisma/client";
 import { SignJWT } from "jose";
+import moment from "moment";
 import { cookies } from "next/headers";
 
 export async function setTokenCookie(userId: string) {
+  const expiry = moment().add({ days: 28 });
   const token = await new SignJWT()
     .setSubject(userId)
     .setProtectedHeader({ alg: "HS256" })
+    .setExpirationTime(expiry.toDate())
     .sign(new TextEncoder().encode(config.JWT_SECRET));
 
-  cookies().set(config.TOKEN_COOKIE_KEY, token, { httpOnly: true, sameSite: true });
+  cookies().set(config.TOKEN_COOKIE_KEY, token, {
+    httpOnly: true,
+    sameSite: true,
+    expires: expiry.toDate()
+  });
 }
