85e355d15689c3128679a62f467beb91a251ad45
Shivang Rathore <rshivang12345@gmail.com>
2024-05-04 21:47:17 +0530

    fix: theme button logic

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -3,22 +3,27 @@
 import { useTheme } from "next-themes";
 import { Button } from "./ui/button";
 import { MoonIcon, SunIcon } from "lucide-react";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 
 export function ThemeSwitch() {
   const { theme, setTheme } = useTheme();
-  const [isClient, setIsClient] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
-    setIsClient(true);
+    setIsMounted(true);
   }, []);
+
   return (
-    <Button
-      variant="outline"
-      size="icon"
-      onClick={() => setTheme(theme == "dark" ? "light" : "dark")}
-    >
-      {isClient && theme == "dark" ? <SunIcon /> : <MoonIcon />}
-    </Button>
+    <Suspense fallback={null}>
+      <Button
+        variant="outline"
+        size="icon"
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      >
+        {/* prettier-ignore-start */}
+        {isMounted ? theme === "dark" ? <SunIcon /> : <MoonIcon /> : null}
+        {/* prettier-ignore-end */}
+      </Button>
+    </Suspense>
   );
 }
